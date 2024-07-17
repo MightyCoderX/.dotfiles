258e1833528e5adc353b4d593601bc336f23bc23
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2024-07-17 21:48:09 +0200

    Split hyprland config in multiple files with some improvements too

---
+++ .config/hypr/scripts/xdg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sleep 1
+
+# kill all possible running xdg-desktop-portals
+killall xdg-desktop-portal-gnome
+killall xdg-desktop-portal-kde
+killall xdg-desktop-portal-lxqt
+killall xdg-desktop-portal-wlr
+killall xdg-desktop-portal-gtk
+
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -236,6 +236,7 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'brenoprata10/nvim-highlight-colors',
+  'elkowar/yuck.vim',
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
