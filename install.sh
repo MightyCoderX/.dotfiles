95af43017e507399284f219542a46461889df51a
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2024-07-19 07:39:04 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ ask() {
 }
 
 echo -e "\nShell configuration (bash)\n"
-for file in shell/*; do
-    ! ask "Source $file?" && continue
+for local_path in shell/*; do
+    ! ask "Source $local_path?" && continue
     
-    echo "source $(realpath "$file")" >> ~/.bashrc
+    echo "source $(realpath "$local_path")" >> ~/.bashrc
 done
 
 echo -e "\nMisc configs \n"
@@ -20,12 +20,11 @@ configs=(
     '.vimrc'
     '.tmux.conf'
     '.gitconfig'
-    '.config/alacritty/alacritty.toml'
-    '.config/hypr/hyprland.conf'
-    '.config/hypr/hyprpaper.conf'
+    '.config/alacritty'
+    '.config/hypr'
 )
-for file in "${configs[@]}"; do
-    ! ask "Install $file?" && continue
+for local_path in "${configs[@]}"; do
+    ! ask "Install $local_path?" && continue
 
-    ln -s "$(realpath "$file")" ~/"$file"
+    ln -s "$(realpath "$local_path")" ~/"$local_path"
 done
