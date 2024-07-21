b67a5fdbfeab5052a1d3081238201f1a6c7ddbeb
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2024-07-21 23:58:24 +0200

    Improved looks and made case insensitive

--- scripts/bemenu.sh
+++ scripts/bemenu.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 
 # Catpuccin Mocha
-if pidof bemenu-run; then
-    killall bemenu-run
-else
-    bemenu-run\
-        -l 5 --fixed-height -c -W 0.5 --fn "Hack Nerd Font 12"\
-        -p run --single-instance\
-        --fn "monospace 12" -H 30\
-        --fb "#1e1e2e"\
-        --ff "#cdd6f4"\
-        --nb "#1e1e2e"\
-        --nf "#cdd6f4"\
-        --tb "#1e1e2e"\
-        --hb "#1e1e2e"\
-        --tf "#f38ba8"\
-        --hf "#f9e2af"\
-        --af "#cdd6f4"\
-        --ab "#1e1e2e"
-fi
+
+bemenu-run\
+    -l 5 --fixed-height -c -W 0.5 --fn "Hack Nerd Font 12"\
+    -p run --single-instance --ignorecase\
+    --border 2 --bdr '#89dceb'\
+    --fn "monospace 12" -H 30\
+    --fb "#1e1e2e"\
+    --ff "#cdd6f4"\
+    --nb "#1e1e2e"\
+    --nf "#cdd6f4"\
+    --tb "#1e1e2e"\
+    --hb "#1e1e2e"\
+    --tf "#89dceb"\
+    --hf "#f9e2af"\
+    --af "#cdd6f4"\
+    --ab "#1e1e2e"
+
 
