57f1037a34cd8b5b5e4602b3698f723c3039ada2
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2024-07-22 02:18:00 +0200

    Made background transparent again

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,7 +5,7 @@ return {
 		priority = 1000,
 		config = function()
 			require("catppuccin").setup({
-				transparent_background = false,
+				transparent_background = true,
 			})
 			vim.cmd.colorscheme("catppuccin-mocha")
 		end,
