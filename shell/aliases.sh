5271eefaae5da521b9b0cb2038af59ae1467ac1a
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2024-07-29 23:08:53 +0200

    Add alias to search through man pages with fzf

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -3,3 +3,10 @@ alias ls='ls --color=auto -v'
 alias ll='ls -lAh'
 alias tree='tree -C -I .git'
 alias ip='ip --color=auto'
+
+_alias_fman() {
+    man -k . | tr -d '()' | awk '{ print $1 "." $2 }'| fzf --preview 'unset MANPAGER; man {}' | xargs man
+}
+
+alias fman='_alias_fman'
+
