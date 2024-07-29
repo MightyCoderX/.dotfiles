9dbd8298218aec8513070ad4069b29e36aa5f8a7
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2024-07-29 22:55:32 +0200

    Update telescope
    - improve search grep commands to include hidden files
    - set new/better keymaps

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,8 +5,54 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find_files"})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live_grep" })
+			vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Telescope keymaps" })
+			vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Telescope diagnostics" })
+
+			require("telescope").setup({
+				defaults = {
+					-- configure to use ripgrep
+					vimgrep_arguments = {
+						"rg",
+						"--follow", -- Follow symbolic links
+						"--hidden", -- Search for hidden files
+						"--no-heading", -- Don't group matches by each file
+						"--with-filename", -- Print the file path with the matched lines
+						"--line-number", -- Show line numbers
+						"--column", -- Show column numbers
+						"--smart-case", -- Smart case search
+
+						-- Exclude some patterns from search
+						"--glob=!**/.git/*",
+						"--glob=!**/.idea/*",
+						"--glob=!**/.vscode/*",
+						"--glob=!**/build/*",
+						"--glob=!**/dist/*",
+						"--glob=!**/node_modules/*",
+						"--glob=!**/package-lock.json",
+					},
+				},
+				pickers = {
+					find_files = {
+						hidden = true,
+						-- needed to exclude some files & dirs from general search
+						-- when not included or specified in .gitignore
+						find_command = {
+							"rg",
+							"--files",
+							"--hidden",
+							"--glob=!**/.git/*",
+							"--glob=!**/.idea/*",
+							"--glob=!**/.vscode/*",
+							"--glob=!**/build/*",
+							"--glob=!**/dist/*",
+							"--glob=!**/node_modules/*",
+							"--glob=!**/package-lock.json",
+						},
+					},
+				},
+			})
 		end,
 	},
 	{
