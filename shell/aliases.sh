7e18d17a191e78d07387fbf5d15ce5c6d2eb13ee
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2024-08-03 18:02:42 +0200

    Add alias for zoxide

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -3,6 +3,7 @@ alias ls='ls --color=auto -v'
 alias ll='ls -lAh'
 alias tree='tree -C -I .git'
 alias ip='ip --color=auto'
+alias cd='z'
 
 _alias_fman() {
     man -k . | tr -d '()' | awk '{ print $1 "." $2 }'| fzf --preview 'unset MANPAGER; man {}' | xargs man
