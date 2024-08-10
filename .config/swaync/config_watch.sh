072a893ecfd94c506402c2bec2f061acc0205e4d
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2024-08-11 01:35:06 +0200

    Improve swaync config
    Make it modular and add keybind to open control center

--- .config/swaync/config_watch.sh
+++ .config/swaync/config_watch.sh
@@ -8,10 +8,6 @@ inotifywait -e close_write -m . |\
 while read -r _ events filename; do
     [ "$events" != "CLOSE_WRITE,CLOSE" ] && continue
 
-    if [ "$filename" != "config.json" ] && [ "$filename" != "style.css" ]; then 
-        continue
-    fi
-
     echo "$events $filename"
     swaync-client --reload-css
     swaync-client --reload-config
