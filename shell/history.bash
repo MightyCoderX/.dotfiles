1131b01df114cf5d216f978b9fc10238f92e363c
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2024-08-26 01:41:46 +0200

    Update history shell config to move clean up home folder

--- shell/history.bash
+++ shell/history.bash
@@ -4,5 +4,6 @@ HISTCONTROL='ignoreboth'
 HISTSIZE=-1
 HISTFILESIZE=-1
 HISTTIMEFORMAT='[%F %T] '
-HISTFILE=~/.bash_eternal_history
+HISTFILE="$XDG_STATE_HOME"/bash/history
 PROMPT_COMMAND="history -a; $PROMPT_COMMAND" # Append history to HISTFILE before each command
+
