9c7dfda40b0b37f482b478fcd660359a97fc5405
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2024-11-10 09:58:27 +0100

    Improving install script (WIP)

--- install.sh
+++ install.sh
@@ -6,35 +6,37 @@ ask() {
     [ -z "$resp" ] || [ "$resp" = "y" ]
 }
 
-rc_file=""
+envsubst
 
 case "$(basename "$SHELL")" in
     bash)
-        rc_file+="$HOME/.bashrc"
+        install_bash
         ;;
     zsh)
-        rc_file+="$HOME/.zshrc"
+        install_zsh
         ;;
     *)
         echo "Detected unknown shell '$SHELL'"
         ;;
 esac
 
-while [ ! -w "$rc_file" ]; do
-    echo "rc file does not exist or is not writeable"
-    read -rp "rc file path: \n" rc_file_name
-    rc_file+="$rc_file_name"
-done
+install_bash() {
+    mv .bashrc
+}
 
-echo -e "\nShell configs (shell dependent)\n"
+install_zsh() {
+    :
+}
+
+echo -e "\nShell extras (based on $$SHELL)\n"
 for local_path in shell/*.{"$(basename "$SHELL")",sh}; do
     ! ask "Source $local_path?" && continue
-    
+
     echo "source $(realpath "$local_path")" >> "$rc_file"
 done
 
 echo -e "\nMisc configs \n"
-for local_path in $(find .config home -mindepth 1 -maxdepth 1); do
+find .config home -mindepth 1 -maxdepth 1 | while read -r local_path; do
     if [ "$(dirname "$local_path")" = "home" ]; then
         local_path="$(basename "$local_path")"
     fi
