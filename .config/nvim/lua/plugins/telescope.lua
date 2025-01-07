189b90529335dc3ccaa4f68c5937d13d828c44ca
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2025-01-07 22:08:21 +0100

    Change bind for file search

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find_files"})
+			vim.keymap.set("n", "<leader><leader>", builtin.find_files, { desc = "Telescope find_files"})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live_grep" })
 			vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Telescope keymaps" })
 			vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Telescope diagnostics" })
