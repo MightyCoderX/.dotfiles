f6c06a84a4fed047a89cd06ce96a4b919e60e6d6
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2025-01-07 22:06:13 +0100

    Update aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -4,9 +4,13 @@
 # alias tree='tree -C -I .git'
 alias ip='ip --color=auto'
 alias cd='z'
-alias ls='eza -l --icons=auto'
+alias ls='eza -lg --icons=auto'
 alias ll='ls -a'
-alias tree='eza -l --icons=auto --tree'
+alias l.='ls .*'
+alias tree='eza -lg --icons=auto --tree'
+alias tz="tz -w -m"
+alias grep="grep --color"
+alias rm="trash"
 
 _alias_fman() {
     man -k . | tr -d '()' | awk '{ print $1 "." $2 }'| fzf --preview 'unset MANPAGER; man {}' | xargs man
