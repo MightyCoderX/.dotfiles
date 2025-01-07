4c91aa74c7ac6148eba56bb1fb2cf5832db5630a
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2025-01-07 22:08:05 +0100

    Remove duplicated code (how?)

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -9,6 +9,17 @@ return {
         build = "cargo build --release",
         enabled = false
     },
+    {  "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+        config = function()
+            require("lsp_lines").setup()
+            vim.keymap.set(
+              "",
+              "<Leader>l",
+              require("lsp_lines").toggle,
+              { desc = "Toggle lsp_lines" }
+            )
+        end,
+    },
     {
         "williamboman/mason-lspconfig.nvim",
         opts = {
@@ -25,6 +36,8 @@ return {
                 "html",
                 "emmet_ls",
                 "hyprls",
+                "cmake",
+                "dockerls"
             },
         },
     },
@@ -34,35 +47,11 @@ return {
             local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
             require("mason-lspconfig").setup_handlers({
-                function (server_name) 
+                function (server_name)
                     require("lspconfig")[server_name].setup({
                         capabilities = lsp_capabilities
                     })
                 end
-            })
-
-            local _border = "single"
-            require("lspconfig.ui.windows").default_options.border = _border
-
-            vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-                border = _border,
-            })
-            vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-                border = _border,
-            })
-            vim.diagnostic.config({
-                float = { border = _border },
-            })
-
-            vim.api.nvim_create_autocmd("LspAttach", {
-                group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-                callback = function(ev)
-                    local opts = { buffer = ev.buf }
-                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-                    vim.keymap.set({ "n", "v" }, "<leader>cr", vim.lsp.buf.rename, opts)
-                end,
             })
 
             local _border = "single"
