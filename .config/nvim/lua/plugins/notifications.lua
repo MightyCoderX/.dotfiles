46784bfd48c3d4c52f09d6bf95abef3c3e5b43fb
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2025-01-15 23:16:26 +0100

    Fix wrong setting path

--- .config/nvim/lua/plugins/notifications.lua
+++ .config/nvim/lua/plugins/notifications.lua
@@ -1,10 +1,10 @@
 return {
     "j-hui/fidget.nvim",
     opts = {
-        window = { winblend = 0 },
+        notification = { window = { winblend = 0 } },
         progress = {
             display = { group_style = "@string" },
-            lsp = { progress_ringbuf_size = 1024 }
-        }
+            lsp = { progress_ringbuf_size = 1024 },
+        },
     },
 }
