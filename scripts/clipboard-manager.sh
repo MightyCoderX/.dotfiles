aac0eb7b4aff70704efc54aa4c2830c174e3722d
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2025-01-23 02:30:04 +0100

    Update prompt for clipboard menu

--- scripts/clipboard-manager.sh
+++ scripts/clipboard-manager.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cliphist list\
-    | rofi -dmenu -p "clipboard" -theme-str "window { width: 60%; } listview { columns: 1; }"\
+    | rofi -dmenu -p "Clipboard" -theme-str "window { width: 60%; } listview { columns: 1; }"\
     | cliphist decode | wl-copy
