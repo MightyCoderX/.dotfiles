428071887f756a5887f823e90362c28483411c29
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2025-01-23 02:29:12 +0100

    Fix deprecated practice

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,7 +1,7 @@
 return {
     "nvim-lualine/lualine.nvim",
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = {
+    opts = {
         extensions = { "neo-tree" },
         sections = {
             lualine_c = { { "filename", path = 1 } }
