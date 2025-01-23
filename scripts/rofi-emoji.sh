cda6bacd3cf790f3304149fa6225649a2857390a
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2025-01-23 02:30:30 +0100

    Use custom prompt for emoji menu

--- scripts/rofi-emoji.sh
+++ scripts/rofi-emoji.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-unipicker --command 'rofi -theme-str "window { width: 50%; height: 60%; } listview { columns: 1; }" -dmenu'\
+unipicker --command 'rofi -theme-str "window { width: 50%; height: 60%; } listview { columns: 1; }" -dmenu -p Emoji'\
     | wl-copy
