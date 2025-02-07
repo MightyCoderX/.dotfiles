4d2418cdd9acff8f5ad2dbdbb6d246875e284adb
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2025-02-07 03:01:09 +0100

    Add fix for accidental link clicks and disable opacity

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -20,9 +20,31 @@ config.bold_brightens_ansi_colors = false
 
 config.enable_tab_bar = false
 
-config.window_background_opacity = 0.9
+config.window_background_opacity = 1
 
 -- config.text_background_opacity = 0
+
+-- Fix accidental URL clicks
+config.mouse_bindings = {
+    -- Disable the default click behavior
+    {
+      event = { Up = { streak = 1, button = "Left"} },
+      mods = "NONE",
+      action = wezterm.action.CompleteSelection 'PrimarySelection', -- You may want a different option here. See /u/Brian's comment below
+    },
+    -- Ctrl-click will open the link under the mouse cursor
+    {
+        event = { Up = { streak = 1, button = "Left" } },
+        mods = "CTRL",
+        action = wezterm.action.OpenLinkAtMouseCursor,
+    },
+    -- Disable the Ctrl-click down event to stop programs from seeing it when a URL is clicked
+    {
+        event = { Down = { streak = 1, button = "Left" } },
+        mods = "CTRL",
+        action = wezterm.action.Nop,
+    },
+}
 
 return config
 
