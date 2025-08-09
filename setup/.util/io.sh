c63619f6f6a1c9a6c95b12c9bd05159a71c4275e
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2025-08-09 11:51:45 +0200

    Fix printf commands to use correct format to preserve escapes

--- setup/.util/io.sh
+++ setup/.util/io.sh
@@ -2,22 +2,22 @@
 DOTFILES_UTIL_IO=1
 
 info() {
-    printf '[INFO] %s\n' "$*"
+	printf '[INFO] %b\n' "$*"
 }
 
 warn() {
-    printf '[WARN] %s\n' "$*" >&2
+	printf '[WARN] %b\n' "$*" >&2
 }
 
 fatal() {
-    printf '[FATAL] %s\n' "$*" >&2
-    exit 1
+	printf '[FATAL] %b\n' "$*" >&2
+	exit 1
 }
 
 ask() {
-    resp="n"
-    read -rp "$1 (y/N): " </dev/tty
-    [ -n "$REPLY" ] && resp="$REPLY"
+	resp="n"
+	read -rp "$1 (y/N): " </dev/tty
+	[ -n "$REPLY" ] && resp="$REPLY"
 
-    [ "$resp" = "y" ]
+	[ "$resp" = "y" ]
 }
