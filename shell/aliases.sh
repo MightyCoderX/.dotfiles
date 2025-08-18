d2650781deeaa466cdbfe1122e3cb835863cf4b6
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2025-08-18 12:01:40 +0200

    Create function for complex alias

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -5,11 +5,15 @@ alias tree='tree -C -I .git'
 alias ip='ip --color=auto'
 alias grep="grep --color"
 
+# Search man with fzf
 _alias_fman() {
 	man -k . | tr -d '()' | awk '{ print $1 "." $2 }' | fzf --preview 'unset MANPAGER; man {}' | xargs man
 }
-
 alias fman='_alias_fman'
 
 # List all readline keybinds
-alias bindl="(printf 'COMMAND\tKEYBIND(s)' && bind -P | grep -v 'is not bound' | sed 's/can be found on //') | column -t -l 2"
+_alias_bindl() {
+	{ printf 'COMMAND\tKEYBIND(s)' && bind -P | grep -v 'is not bound' | sed 's/can be found on //'; } |
+		column -t -l 2
+}
+alias bindl='_alias_bindl'
