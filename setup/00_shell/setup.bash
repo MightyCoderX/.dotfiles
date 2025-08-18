699c8a367bf801fa5e2796620f9fe7302d02f667
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2025-08-18 21:59:49 +0200

    Use new system for setup/00_shell/setup.bash

--- setup/00_shell/setup.bash
+++ setup/00_shell/setup.bash
@@ -1,28 +1,53 @@
-DOTFILES_SHELL=""
-while [[ -z "$DOTFILES_SHELL" ]]; do
-	read -rp "Select shell (bash/zsh): "
-	case "$REPLY" in
-	bash | zsh)
-		DOTFILES_SHELL="$REPLY"
+setup_shell_configs() {
+	ask "Source shell configurations in respective rc files?" || return
+
+	info "Sourcing shell configurations"
+
+	[[ ! -d ./shell ]] && warn "Directory ./shell not found, skipping" && return
+
+	for shell_conf in ./shell/*.{sh,bash}; do
+		echo "source $(realpath "$shell_conf")" >>~/.bashrc
+	done
+
+	[[ -f ~/.zshrc ]] && for shell_conf in ./shell/*.{sh,zsh}; do
+		echo "source $(realpath "$shell_conf")" >>~/.zshrc
+	done
+
+	info "Sourced all shell configurations"
+
+	echo
+}
+
+setup() {
+	DOTFILES_SHELL=""
+	while [[ -z "$DOTFILES_SHELL" ]]; do
+		read -rp "Select shell (bash/zsh): "
+		case "$REPLY" in
+		bash | zsh)
+			DOTFILES_SHELL="$REPLY"
+			;;
+		esac
+	done
+
+	case "$DOTFILES_SHELL" in
+	bash)
+		DOTFILES_RC_FILE=~/.bashrc
+		;;
+	zsh)
+		DOTFILES_RC_FILE=~/.zshrc
 		;;
 	esac
-done
 
-case "$DOTFILES_SHELL" in
-bash)
-	DOTFILES_RC_FILE=~/.bashrc
-	;;
-zsh)
-	DOTFILES_RC_FILE=~/.zshrc
-	;;
-esac
+	if [[ ! "$DOTFILES_RC_FILE" ]]; then
+		fatal "rc file path needed to run setup scripts"
+	fi
 
-if [[ ! "$DOTFILES_RC_FILE" ]]; then
-	fatal "rc file path needed to run setup scripts"
-fi
+	if [[ ! -w "$DOTFILES_RC_FILE" ]]; then
+		fatal "rc file '$DOTFILES_RC_FILE' doesn't exist or is not writeable"
+	fi
 
-run_setup "$SCRIPT_DIR"/"$DOTFILES_SHELL"
+	run_setup "$SCRIPT_DIR"/"$DOTFILES_SHELL"
 
-if [[ ! -w "$DOTFILES_RC_FILE" ]]; then
-	fatal "rc file '$DOTFILES_RC_FILE' doesn't exist or is not writeable"
-fi
+	setup_shell_configs
+	unset -f setup_shell_configs
+}
