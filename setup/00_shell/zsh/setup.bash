7566285c4b4e359331c1c0c05f0baeeaf00014fb
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2025-08-18 21:59:33 +0200

    Add .zshrc copying

--- setup/00_shell/zsh/setup.bash
+++ setup/00_shell/zsh/setup.bash
@@ -1,8 +1,10 @@
 setup() {
 	install_pacman zsh
 	install_dnf zsh
-
-	zsh
 }
-#TODO install zinit plugin manager and add stuff to .zshrc
+
 #TODO move zsh files into XDG_ directories
+
+config() {
+	cp "$DOTFILES_PATH"/home/.zshrc ~/.zshrc
+}
