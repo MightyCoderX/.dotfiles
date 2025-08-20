da42a3253b80ed45be8c38224face9f074593db9
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2025-08-20 13:13:12 +0200

    Update asmfetch setup to use new system

--- setup/90_asmfetch/setup.bash
+++ setup/90_asmfetch/setup.bash
@@ -1,18 +1,20 @@
-[[ ! -d ~/repo ]] && mkdir ~/repo
+setup() {
+	[[ ! -d ~/repo ]] && mkdir ~/repo
 
-REPO_PATH=~/repo/asmfetch
+	REPO_PATH=~/repo/asmfetch
 
-[[ -d "$REPO_PATH" ]] && run rm -rf "$REPO_PATH"
-run git clone https://github.com/ErrorNoInternet/asmfetch "$REPO_PATH"
+	[[ -d "$REPO_PATH" ]] && run rm -rf "$REPO_PATH"
+	run git clone https://github.com/ErrorNoInternet/asmfetch "$REPO_PATH"
 
-# Build and Install
-pushd "$REPO_PATH" || exit 1
+	# Build and Install
+	pushd "$REPO_PATH" || exit 1
 
 echo "$DISTRO_ID"
 if ! ${CONFIG[dry_run]}; then
 	sed -iE 's?(\.include "logo/).*(\.S")?\1'"$DISTRO_ID"'\2?' asmfetch.S
 fi
 
-run gcc -nostdlib -no-pie asmfetch.S -o asmfetch
-run cp asmfetch "$HOME/.local/bin/"
-popd || exit 1
+	run gcc -nostdlib -no-pie asmfetch.S -o asmfetch
+	run cp asmfetch "$HOME/.local/bin/"
+	popd || exit 1
+}
