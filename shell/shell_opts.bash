c520d9e3f1742bc88715ca3d8c7ede921e34ce3b
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2025-08-22 21:19:40 +0200

    Set bash options to avoid weird behaviour with globs

--- shell/shell_opts.bash
+++ shell/shell_opts.bash
@@ -1,2 +1,6 @@
-shopt -s autocd     # Enable autocd to change dir by just typing the path
-set -o noclobber    # Disable overriding files with redirection
+shopt -s autocd   # Enable autocd to change dir by just typing the path
+shopt -s nullglob # Enable nullglob so that globs that don't match files don't return the glob string but return an empty one
+shopt -s globstar # Enable globstar which allows for recursive globbing with **
+#shopt -s dotglob  # Enable dotglob which includes dotfiles in * glob
+
+set -o noclobber # Disable overriding files with redirection
