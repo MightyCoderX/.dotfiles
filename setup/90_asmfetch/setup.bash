b4a015eaea53c3fa7a268b464883cad5ad6a2831
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2025-08-27 23:41:02 +0200

    Improve asmfetch setup by putting all commands in run block

--- setup/90_asmfetch/setup.bash
+++ setup/90_asmfetch/setup.bash
@@ -7,13 +7,12 @@ setup() {
 	run git clone https://github.com/ErrorNoInternet/asmfetch "$REPO_PATH"
 
 	# Build and Install
-	pushd "$REPO_PATH" || exit 1
 
 	run <<-EOF
+		pushd "$REPO_PATH" || return 1
 		sed -i -E 's|(\.include "logo/).*(\.S")|\1'"$DISTRO_ID"'\2|' asmfetch.S
+		gcc -nostdlib -no-pie asmfetch.S -o asmfetch
+		cp asmfetch "$HOME/.local/bin/"
+		popd || return 1
 	EOF
-
-	run gcc -nostdlib -no-pie asmfetch.S -o asmfetch
-	run cp asmfetch "$HOME/.local/bin/"
-	popd || exit 1
 }
