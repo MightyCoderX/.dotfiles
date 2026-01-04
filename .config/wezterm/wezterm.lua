46d9d891903e878f7e24ade4381714a7b17f09e3
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2026-01-04 14:23:15 +0100

    Add binding to change colorscheme when in blue and gray ncurses screens for better contrast

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,10 +7,45 @@ config.default_prog = { "/usr/bin/fish" }
 local custom = wezterm.color.get_builtin_schemes()["Catppuccin Frappe"]
 custom.background = "#11111b" -- crust
 
+local function deep_copy(obj)
+	if type(obj) ~= "table" then
+		return obj
+	end
+	local res = {}
+	for k, v in pairs(obj) do
+		res[k] = deep_copy(v)
+	end
+	return res
+end
+
+local ncurses_friendly = deep_copy(custom)
+ncurses_friendly.ansi[5] = "#1e66f5"
+ncurses_friendly.brights[5] = "#89b4fa"
+
 config.color_schemes = {
-    ["Catppuccin Espresso"] = custom
+	["Catppuccin Espresso"] = custom,
+	["Catppuccin Espresso NCurses"] = ncurses_friendly,
 }
 config.color_scheme = "Catppuccin Espresso"
+
+-- Add keybinding to toggle color scheme
+config.keys = {
+	{
+		key = "T",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.EmitEvent("toggle-colorscheme"),
+	},
+}
+
+wezterm.on("toggle-colorscheme", function(window, _)
+	local overrides = window:get_config_overrides() or {}
+	if overrides.color_scheme == "Catppuccin Espresso NCurses" then
+		overrides.color_scheme = "Catppuccin Espresso"
+	else
+		overrides.color_scheme = "Catppuccin Espresso NCurses"
+	end
+	window:set_config_overrides(overrides)
+end)
 
 config.font = wezterm.font_with_fallback({
 	"MesloLGS Nerd Font",
