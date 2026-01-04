2e16d57de90d499373813e8b3109d68b961def40
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2026-01-04 14:21:59 +0100

    Fix formatting

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local config = {}
 
-config.default_prog = { '/usr/bin/fish' }
+config.default_prog = { "/usr/bin/fish" }
 
 local custom = wezterm.color.get_builtin_schemes()["Catppuccin Frappe"]
 custom.background = "#11111b" -- crust
@@ -13,8 +13,8 @@ config.color_schemes = {
 config.color_scheme = "Catppuccin Espresso"
 
 config.font = wezterm.font_with_fallback({
-    "MesloLGS Nerd Font",
-    "Font Awesome 6 Free Solid"
+	"MesloLGS Nerd Font",
+	"Font Awesome 6 Free Solid",
 })
 config.font_size = 12
 config.bold_brightens_ansi_colors = false
@@ -27,25 +27,24 @@ config.window_background_opacity = 1
 
 -- Fix accidental URL clicks
 config.mouse_bindings = {
-    -- Disable the default click behavior
-    {
-      event = { Up = { streak = 1, button = "Left"} },
-      mods = "NONE",
-      action = wezterm.action.CompleteSelection 'PrimarySelection', -- You may want a different option here. See /u/Brian's comment below
-    },
-    -- Ctrl-click will open the link under the mouse cursor
-    {
-        event = { Up = { streak = 1, button = "Left" } },
-        mods = "CTRL",
-        action = wezterm.action.OpenLinkAtMouseCursor,
-    },
-    -- Disable the Ctrl-click down event to stop programs from seeing it when a URL is clicked
-    {
-        event = { Down = { streak = 1, button = "Left" } },
-        mods = "CTRL",
-        action = wezterm.action.Nop,
-    },
+	-- Disable the default click behavior
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "NONE",
+		action = wezterm.action.CompleteSelection("PrimarySelection"), -- You may want a different option here. See /u/Brian's comment below
+	},
+	-- Ctrl-click will open the link under the mouse cursor
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = wezterm.action.OpenLinkAtMouseCursor,
+	},
+	-- Disable the Ctrl-click down event to stop programs from seeing it when a URL is clicked
+	{
+		event = { Down = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = wezterm.action.Nop,
+	},
 }
 
 return config
-
