ea3ea9f93947fb0316d00a85d00db18dd49d8d6c
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2026-06-04 15:31:42 +0200

    Use tabs in shell scripts, also remove telescope references since it's not included in lazyvim anymore

--- scripts/tmux-sessions/dotfiles.sh
+++ scripts/tmux-sessions/dotfiles.sh
@@ -3,42 +3,41 @@
 session="dotfiles"
 
 if tmux list-sessions | grep -q "$session"; then
-    tmux attach-session -t "$session"
-    exit 1
+	tmux attach-session -t "$session"
+	exit 1
 fi
 
 _new_window() {
-    window_name="$1"
-    command="$2"
+	window_name="$1"
+	command="$2"
 
-    window_index=$(( window_index + 1 ))
-    tmux new-window -t "$session:$window_index" -n "$window_name" "$command"
+	window_index=$((window_index + 1))
+	tmux new-window -t "$session:$window_index" -n "$window_name" "$command"
 
-    unset window_name command
+	unset window_name command
 }
 
 if ! tmux list-sessions | grep -q "$session"; then
-    root_dir="$HOME/.dotfiles"
-    window_index=1
+	root_dir="$HOME/.dotfiles"
+	window_index=1
 
-    tmux new-session -d -c "$root_dir" -s "$session" "cd $root_dir/.config; nvim . +'Telescope find_files'"
-    tmux rename-window -t "$session:$window_index" '.config'
+	tmux new-session -d -c "$root_dir" -s "$session" "cd $root_dir/.config; nvim ."
+	tmux rename-window -t "$session:$window_index" '.config'
 
-    _new_window 'hypr*' "cd $root_dir/.config/hypr; nvim . +'Telescope find_files'"
-    _new_window 'waybar' "cd $root_dir/.config/waybar/; nvim config.jsonc"
-    _new_window 'swaync' "cd $root_dir/.config/swaync/; nvim config.json"
-    _new_window 'rofi' "cd $root_dir/.config/rofi/; nvim config.rasi"
+	_new_window 'hypr*' "cd $root_dir/.config/hypr; nvim ."
+	_new_window 'waybar' "cd $root_dir/.config/waybar/; nvim config.jsonc"
+	_new_window 'swaync' "cd $root_dir/.config/swaync/; nvim config.json"
+	_new_window 'rofi' "cd $root_dir/.config/rofi/; nvim config.rasi"
 
-    _new_window 'wezterm' "cd $root_dir/.config/wezterm; nvim wezterm.lua"
-    _new_window 'tmux' "cd $root_dir/.config/tmux; nvim tmux.conf"
-    _new_window 'shell' "cd $root_dir/shell; nvim . +'Telescope find_files'"
-    _new_window 'fish' "cd $root_dir/.config/fish; nvim . +'Telescope find_files'"
-    _new_window 'nvim' "cd $root_dir/.config/nvim; nvim . +'Telescope find_files'"
+	_new_window 'wezterm' "cd $root_dir/.config/wezterm; nvim wezterm.lua"
+	_new_window 'tmux' "cd $root_dir/.config/tmux; nvim tmux.conf"
+	_new_window 'shell' "cd $root_dir/shell; nvim ."
+	_new_window 'fish' "cd $root_dir/.config/fish; nvim ."
+	_new_window 'nvim' "cd $root_dir/.config/nvim; nvim ."
 
-    _new_window 'scripts' "cd $root_dir/scripts; nvim . +'Telescope find_files'"
+	_new_window 'scripts' "cd $root_dir/scripts; nvim ."
 
-    _new_window 'lazygit' "lazygit"
+	_new_window 'lazygit' "lazygit"
 
-    tmux attach-session -t "$session"
+	tmux attach-session -t "$session"
 fi
-
